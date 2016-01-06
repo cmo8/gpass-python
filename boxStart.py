690bf3c5e7c1e11f1185eacbe66ca9ce311db7ab
Sean Morgan <cmo.uwp.2010@gmial.com>
2016-01-05 22:03:22 -0600

    working on GPG key generation and creating password store

--- boxStart.py
+++ boxStart.py
@@ -9,6 +9,8 @@ class BoxStart(Gtk.VButtonBox):
         Gtk.VButtonBox.__init__(self)
         self.parent = parent
         self.config = config
+        self.gpg = GPGkey(self.config.gpgbinary, self.config.gpghome)
+
         #self.set_spacing(20)
         self.set_layout(Gtk.ButtonBoxStyle.CENTER)
         self.set_spacing(20)
@@ -28,15 +30,40 @@ class BoxStart(Gtk.VButtonBox):
         self.pack_start(self.btnGitClonePassStore, True, True, 0)
 
     def btnCreatePassStore_clicked(self, button):
-        dialogCreatePassStore = DialogCreatePassStore(self.parent, self.config)
-        response = dialogCreatePassStore.run()
-
-        if response == Gtk.ResponseType.OK:
-            print("OK button clicked")
-        elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel button clicked")
-        else:
-            print("Dialog closed")
+        keys = self.gpg.list_keys()
+        dialogCreatePassStore = DialogCreatePassStore(self.parent, self.config, keys)
+        loop_continue = false
+        while not loop_continue:
+            response = dialogCreatePassStore.run()
+            if response == Gtk.ResponseType.OK:
+                if dialogCreatePassStore.gen_key:
+                    real_name = dialogCreatePassStore.txtKeyName.get_text()
+                    email = dialogCreatePassStore.txtEmail.get_text()
+                    password = dialogCreatePassStore.txtPassword.get_text()
+                    passwordc = dialogCreatePassStore.txtPasswordC.get_text()
+                    if real_name != "" and email != "" and password != "" and passwordc != "" and password == passwordc:
+                        print("Real Name:", real_name)
+                        print("Email:", email)
+                        self.gpg.generate_key(real_name, email, password)
+                        self.config.set_gpgkey(email)
+                        loop_continue = True
+                    else:
+                        print("Can NOT Generate Key!!")
+                else:
+                    key_id = dialogCreatePassStore.selected_key
+                    self.config.set_gpgkey(key_id)
+                self.config.set_password_store(dialogCreatePassStore.txtLocation.get_text())
+                print("Password Store Home:", )
+                os.mkdir(self.config.get_password_store)
+                loop_continue = True
+                print("OK button clicked")
+            elif response == Gtk.ResponseType.CANCEL:
+                loop_continue = True
+                print("Cancel button clicked")
+            else:
+                loop_continue = True
+                print("Dialog closed")
+            pass
 
         dialogCreatePassStore.destroy()
         print('btnCreatePassStore_clicked')
--- dialogCreatePassStore.py
+++ dialogCreatePassStore.py
@@ -1,14 +1,14 @@
 import os
 from gi.repository import Gtk, Gdk
-from gpgkey import GPGkey
 
 class DialogCreatePassStore(Gtk.Dialog):
-
+    self.gen_key = False
     #Constructor
-    def __init__(self, parent, config):
+    def __init__(self, parent, config, keys):
         Gtk.Dialog.__init__(self, "Create Password Store", parent, 10,(Gtk.STOCK_OK, Gtk.ResponseType.OK, Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
-        #self.set_parent(parent)
+        self.parent = parent
         self.config = config
+        self.selected_key = None
         self.set_default_size(400, 300)
         self.set_position(Gtk.WindowPosition.CENTER)
         self.box = self.get_content_area()
@@ -21,16 +21,14 @@ class DialogCreatePassStore(Gtk.Dialog):
         self.lblLocation = Gtk.Label("Select Folder:")
         self.locationGroup.add(self.lblLocation)
         self.txtLocation = Gtk.Entry()
+        self.txtLocation.set_text(self.config.password_store);
         self.locationGroup.add(self.txtLocation)
         self.btnLocationSelect = Gtk.Button("Select")
         self.btnLocationSelect.connect("clicked", self.btnLocationSelect_Clicked)
-        self.locationGroup.add(self.btnLocationSelect)
+        self.locationGroup.pack_start(self.btnLocationSelect, False, False, 10)
 
         self.gpgkeyGroup = Gtk.VBox(10)
         self.vlBox.add(self.gpgkeyGroup)
-        #GPG section
-        self.gpg = GPGkey(self.config.gpgbinary, self.config.gpghome)
-        keys = self.gpg.list_keys()
         if os.path.isdir(self.config.gpghome) and len(keys) > 0:
             self.keysGroup = Gtk.Box(10)
             self.gpgkeyGroup.add(self.keysGroup)
@@ -45,6 +43,7 @@ class DialogCreatePassStore(Gtk.Dialog):
             self.listBoxGPGkey.set_entry_text_column(1)
             self.keysGroup.add(self.listBoxGPGkey)
         else:
+            self.gen_key = True
             #Key name folder
             self.keyNameGroup = Gtk.Box(10)
             self.gpgkeyGroup.add(self.keyNameGroup)
@@ -53,7 +52,7 @@ class DialogCreatePassStore(Gtk.Dialog):
             self.txtKeyName = Gtk.Entry()
             self.keyNameGroup.add(self.txtKeyName)
 
-            #Select Root parent folder
+            #Adding email elements
             self.emailGroup = Gtk.Box(10)
             self.gpgkeyGroup.add(self.emailGroup)
             self.lblemail = Gtk.Label("Email:")
@@ -61,7 +60,7 @@ class DialogCreatePassStore(Gtk.Dialog):
             self.txtEmail = Gtk.Entry()
             self.emailGroup.add(self.txtEmail)
 
-            #Select Root parent folder
+            #Adding Password elements
             self.passwordGroup = Gtk.Box(10)
             self.gpgkeyGroup.add(self.passwordGroup)
             self.lblPassword = Gtk.Label("Password:")
@@ -69,7 +68,7 @@ class DialogCreatePassStore(Gtk.Dialog):
             self.txtPassword = Gtk.Entry()
             self.passwordGroup.add(self.txtPassword)
 
-            #Select Root parent folder
+            #Adding Password confermation elements
             self.passwordCGroup = Gtk.Box(10)
             self.gpgkeyGroup.add(self.passwordCGroup)
             self.lblPasswordC = Gtk.Label("Password:")
@@ -80,12 +79,11 @@ class DialogCreatePassStore(Gtk.Dialog):
             self.btnCreateGPGhome = Gtk.Button("Create GPG Key")
             self.btnCreateGPGhome.connect("clicked", self.btnCreateGPGkey_Clicked)
             self.gpgkeyGroup.add(self.btnCreateGPGhome)
-            #print("No: " + self.config.gpgbinary)
 
         self.show_all()
 
     def btnLocationSelect_Clicked(self, button):
-        filechooserdialog = Gtk.FileChooserDialog(self, title="Open Password Store", buttons=(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+        filechooserdialog = Gtk.FileChooserDialog(self, self.parent, title="Open Password Store", buttons=(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
         filechooserdialog.set_action(Gtk.FileChooserAction.SELECT_FOLDER)
         email_filter = Gtk.FileFilter()
         email_filter.set_name("Folder")
@@ -96,18 +94,18 @@ class DialogCreatePassStore(Gtk.Dialog):
             self.txtLocation.set_text(filechooserdialog.get_filename())
         filechooserdialog.destroy()
 
-    def btnCreateGPGkey_Clicked(self, button):
-        real_name = self.txtKeyName.get_text()
-        email = self.txtEmail.get_text()
-        password = self.txtPassword.get_text()
-        passwordc = self.txtPasswordC.get_text()
-        if real_name != "" and email != "" and password != "" and passwordc != "" and password == passwordc:
-            print("Real Name:", real_name)
-            print("Email:", email)
-            self.gpg.generate_key(real_name, email, password)
-
-        else:
-            print("Can NOT Generate Key!!")
+#    def btnCreateGPGkey_Clicked(self, button):
+#        real_name = self.txtKeyName.get_text()
+#        email = self.txtEmail.get_text()
+#        password = self.txtPassword.get_text()
+#        passwordc = self.txtPasswordC.get_text()
+#        if real_name != "" and email != "" and password != "" and passwordc != "" and password == passwordc:
+#            print("Real Name:", real_name)
+#            print("Email:", email)
+#            self.gpg.generate_key(real_name, email, password)
+#
+#        else:
+#            print("Can NOT Generate Key!!")
 
     def listBoxGPGkey_changed(self, combo):
         tree_iter = combo.get_active_iter()
@@ -115,6 +113,7 @@ class DialogCreatePassStore(Gtk.Dialog):
             model = combo.get_model()
             row_id, name = model[tree_iter][:2]
             print("Selected: ID=%s, name=%s" % (row_id, name))
+            self.selected_key = row_id
         else:
             entry = combo.get_child()
             print("Entered: %s" % entry.get_text())
